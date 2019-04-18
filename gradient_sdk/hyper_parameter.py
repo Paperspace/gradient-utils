9b56581ff12263f19ad4a9a631ae7a7d78abbee0
Paweł Mudlaff <mudlaff.p@gmail.com>
2019-04-18 18:33:43 +0200

    PS-9776 feature: - Change function that is called in hyper_tune

--- gradient_sdk/hyper_parameter.py
+++ gradient_sdk/hyper_parameter.py
@@ -5,7 +5,7 @@ from pymongo.errors import ServerSelectionTimeoutError
 from utils import get_mongo_conn_str, _experiment_name
 
 
-def hyper_tune(train_model, hparam_def, algo=tpe.suggest, max_evals=25, function="fmin"):
+def hyper_tune(train_model, hparam_def, algo=tpe.suggest, max_evals=25):
     """
     Function to prepare and run hyper parameter tune.
 
@@ -13,15 +13,9 @@ def hyper_tune(train_model, hparam_def, algo=tpe.suggest, max_evals=25, function
     :param hparam_def: User hyper tune param definition
     :param algo: Search algorithm
     :param max_evals: Allow up to this many function evaluations before returning
-    :param function: Function that will be used for hyper parameter tune
+
     :return: None if there is no result or dict with result for tune
     """
-    hyper_func_mapping = {
-        "fmin": fmin
-    }
-
-    hyper_func = hyper_func_mapping.get(function)
-
     mongo_connect_str = get_mongo_conn_str()
 
     # TODO better handling of connection to mongo
@@ -31,4 +25,4 @@ def hyper_tune(train_model, hparam_def, algo=tpe.suggest, max_evals=25, function
         except ServerSelectionTimeoutError:
             pass
         else:
-            return hyper_func(train_model, space=hparam_def, trials=trials, algo=algo, max_evals=max_evals)
+            return fmin(train_model, space=hparam_def, trials=trials, algo=algo, max_evals=max_evals)
