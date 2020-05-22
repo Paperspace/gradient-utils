88b74c6f33e8b5f8b3cca928bcb3619fb52bff61
Silas Sewell <silas@paperspace.com>
2020-05-22 10:58:56 -0400

    Set placeholder version and add comment

--- gradient_utils/__init__.py
+++ gradient_utils/__init__.py
@@ -7,7 +7,8 @@ from gradient_utils.multi_node import get_tf_config
 from gradient_utils.utils import get_mongo_conn_str, data_dir, worker_hosts, export_dir, job_name, model_dir, ps_hosts, \
     task_index
 
-__version__ = "0.1.2"
+# this is automatically set to the latest tagged version by CircleCI
+__version__ = "0.0.1"
 
 __all__ = [
     "__version__",
