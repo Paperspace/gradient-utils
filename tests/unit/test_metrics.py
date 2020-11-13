deee1078830b5b3891cfdea525163212f79ef6d0
Roger Lam <roger@paperspace.com>
2020-11-13 14:21:03 -0500

    Add unit tests for Metric class

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,7 +1,8 @@
 import mock
 import pytest
 
-from gradient_utils.metrics import get_metric_pushgateway, CollectorRegistry, add_metrics
+from gradient_utils.metrics import get_metric_pushgateway, CollectorRegistry, add_metrics, Metric
+
 
 def test_add_metrics_errors_with_nonstring_key():
     registry = CollectorRegistry()
@@ -30,3 +31,37 @@ def test_add_metrics_errors_with_nonnumber_value():
     with pytest.raises(ValueError) as ce:
         add_metrics(metrics_obj)
     assert "Value of a metric can only be a number" in str(ce.value)
+
+# Metric
+
+
+def test_metric_creates_object():
+    metric = Metric('key', 100)
+    assert 'key' == metric.key
+    assert 100 == metric.value
+
+
+def test_metric_invalid_key():
+    with pytest.raises(ValueError) as e:
+        metric = Metric(100, 100)
+    assert "Key of a metric can only be a string" in str(e.value)
+
+
+def test_metric_invalid_value():
+    with pytest.raises(ValueError) as e:
+        metric = Metric('key', 'value')
+    assert "Value of a metric can only be a number" in str(e.value)
+
+
+def test_metric_invalid_reassign_key():
+    metric = Metric('key', 100)
+    with pytest.raises(ValueError) as e:
+        metric.key = 1999
+    assert "Key of a metric can only be a string" in str(e.value)
+
+
+def test_metric_invalid_reassign_value():
+    metric = Metric('key', 100)
+    with pytest.raises(ValueError) as e:
+        metric.value = 'value'
+    assert "Value of a metric can only be a number" in str(e.value)
