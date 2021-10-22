bb1c8bbd632e355f94c60bbd9c12143169dfbd30
Christopher Cedeno <chrisrey95@gmail.com>
2021-10-22 12:48:44 -0400

    remove tensorboard import

--- gradient_utils/metrics/__init__.py
+++ gradient_utils/metrics/__init__.py
@@ -1,16 +1,11 @@
 """
 Gradient ML SDK
 """
-from gradient_utils.metrics import tensorboard
 from .metrics import MetricsLogger, add_metrics
 from .init import init
-
-# record of patched libraries
-patched = {"tensorboard": []}
 
 __all__ = [
     "init",
     "add_metrics",
     "MetricsLogger",
-    "tensorboard"
 ]
